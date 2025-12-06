9378d8005d281caf6b947e075ee6c5e279310fbf
André Möll <andre.moell@stud.hslu.ch>
2025-12-06 16:30:51 +0100

    :zap: Day06

--- src/Day06/Main.java
+++ src/Day06/Main.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
