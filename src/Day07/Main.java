e3dac08c1f40ab74cdc593192ee3f9f5c03cf70e
André Möll <andre.moell@stud.hslu.ch>
2025-12-07 13:54:55 +0100

    :zap: Day07

---
+++ src/Day07/Main.java
@@ -0,0 +1,56 @@
+package Day07;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        URL path = Day07.Main.class.getResource("Input.txt");
+        File f = new File(path.getFile());
+        String previousLine = "";
+        List<String> full = new ArrayList<>();
+        int total = 0;
+        try (Scanner scanner = new Scanner(f)) {
+            while (scanner.hasNextLine()) {
+                System.out.println("prev " + previousLine);
+                char[] prevArray = previousLine.toCharArray();
+                String line = scanner.nextLine();
+                char[] lineArray = line.toCharArray();
+                System.out.println("curr " + line);
+
+                if (previousLine.contains("S")) {
+                    int index = previousLine.indexOf("S");
+                    lineArray[index] = '|';
+                    line = String.valueOf(lineArray);
+                } else if (!previousLine.isBlank()) {
+                    for (int i = 0; i < lineArray.length; i++) {
+                        if (lineArray[i] == '^' && prevArray[i] == '|') {
+                            lineArray[i-1] = '|';
+                            lineArray[i+1] = '|';
+                            line = String.valueOf(lineArray);
+                            total++;
+                        } else if (prevArray[i] == '|') {
+                            lineArray[i] = '|';
+                            line = String.valueOf(lineArray);
+                        } else {
+                        }
+                    }
+                }
+
+                previousLine = line;
+                System.out.println("newp " + previousLine);
+                full.add(previousLine);
+                System.out.println();
+            }
+        }
+
+        for (String s : full) {
+            System.out.println(s);
+        }
+        System.out.println("\nTotal is " + total);
+    }
+}
